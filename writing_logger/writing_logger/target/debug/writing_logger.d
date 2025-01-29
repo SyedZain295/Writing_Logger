fae452b679f0714af4389615bb18de29db54e4cd
Syed Zain <syedzaincc64836483@Syeds-MacBook-Air.local>
2025-01-30 00:25:34 +0100

    Initial commit: Added Cargo.toml and main.rs

--- writing_logger/writing_logger/target/debug/writing_logger.d
+++
@@ -1,1 +0,0 @@
-/Users/syedzaincc64836483/writing_logger/writing_logger/writing_logger/target/debug/writing_logger: /Users/syedzaincc64836483/writing_logger/writing_logger/writing_logger/src/main.rs
